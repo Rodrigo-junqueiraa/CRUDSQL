cbdf8076551e0e978d823b6c8f7fd6b35d10c6e8
Rodrigo-junqueiraa <rodrigojunqueira9@gmail.com>
2025-02-26 17:22:55 -0300

    Adiciona formulário de usuários e substitui biblioteca de notificações

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,37 @@
+import GlobalStyle from "./styles/global";
+import styled from "styled-components";
+import Form from "./components/Form.js";
+
+import "react-toastify/dist/ReactToastify.css";
+
+
+const Container = styled.div`
+width: 100%;
+max-width: 800px;
+margin-top: 20px;
+display: flex;
+flex-direction: column;
+align-items: center;
+gap: 10px;
+`;
+
+const Title = styled.h2``;
+
+
+
+
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Container>
+        <Title>USUÁRIOS</Title>
+        <Form />
+      </Container>
+      
+      <GlobalStyle />
+    </>
   );
 }
+
 
 export default App;
